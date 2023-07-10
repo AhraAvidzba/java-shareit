0a060fb18f2a86a3de5dd67b51a175edc8fb81da
AkhraAvidzba <akhraa1@yandex.ru>
2023-07-10 21:16:57 +0300

    feat: реализовать тесты сериализации

---
+++ src/test/java/ru/practicum/shareit/item/dto/ItemDescriptionDtoTest.java
@@ -0,0 +1,30 @@
+package ru.practicum.shareit.item.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ItemDescriptionDtoTest {
+
+    @Autowired
+    private JacksonTester<ItemDescriptionDto> json;
+
+    @Test
+    void testItemDescriptionDto() throws Exception {
+        ItemDescriptionDto itemDescriptionDto = new ItemDescriptionDto();
+        itemDescriptionDto.setId(1L);
+        itemDescriptionDto.setName("отвертка");
+        itemDescriptionDto.setOwnerId(1L);
+
+        JsonContent<ItemDescriptionDto> result = json.write(itemDescriptionDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("отвертка");
+        assertThat(result).extractingJsonPathNumberValue("$.ownerId").isEqualTo(1);
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/item/dto/ItemDtoTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ItemDtoTest {
+
+    @Autowired
+    private JacksonTester<ItemDto> json;
+
+    @Test
+    void testItemDescriptionDto() throws Exception {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setId(1L);
+        itemDto.setName("отвертка");
+        itemDto.setDescription("крестовая");
+        itemDto.setAvailable(true);
+        itemDto.setOwner(1L);
+        itemDto.setRequestId(1L);
+
+        JsonContent<ItemDto> result = json.write(itemDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.name").isEqualTo("отвертка");
+        assertThat(result).extractingJsonPathStringValue("$.description").isEqualTo("крестовая");
+        assertThat(result).extractingJsonPathBooleanValue("$.available").isEqualTo(true);
+        assertThat(result).extractingJsonPathNumberValue("$.owner").isEqualTo(1);
+        assertThat(result).extractingJsonPathNumberValue("$.requestId").isEqualTo(1);
+    }
+}
